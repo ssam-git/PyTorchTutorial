5bcd3e9f59a0b6764ae93b4d4c65c98ed496c5a3
ssam-git <ssamandar@hotmail.com>
2026-02-04 16:03:02 +1100

    Guessed the dataset (MNIST) and reused the code!

--- PyTorchDatasetsAndLoaders.py
+++ PyTorchDatasetsAndLoaders.py
@@ -5,6 +5,13 @@ from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
 
+digit_training_data = datasets.MNIST(    
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
@@ -31,13 +38,29 @@ labels_map = {
     8: "Bag",
     9: "Ankle Boot",
 }
+
+digit_labels_map = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+}
+
 figure = plt.figure(figsize=(8, 8))
 cols, rows = 3, 3
 for i in range(1, cols * rows + 1):
-    sample_idx = torch.randint(len(training_data), size=(1,)).item()
-    img, label = training_data[sample_idx]
+    sample_idx = torch.randint(len(digit_training_data), size=(1,)).item()
+    # print(sample_idx)
+    img, label = digit_training_data[sample_idx]
+    print(digit_training_data[sample_idx])
     figure.add_subplot(rows, cols, i)
-    plt.title(labels_map[label])
+    plt.title(digit_labels_map[label])
     plt.axis("off")
-    plt.imshow(img.squeeze(), cmap="gray")
+    plt.imshow(img.squeeze()) #, cmap="gray")
 plt.show()
